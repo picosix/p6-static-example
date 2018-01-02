ea0a05906d5c3ffc88a6d46b91db0856f7e6f16a
picosix <picosix.com@gmail.com>
2018-01-02 21:51:54 +0700

    Upload image with multer

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const _ = require('lodash');
+const path = require('path');
+const multer = require('multer');
 
 const app = express();
 
@@ -10,6 +12,35 @@ app.get('/', (req, res) =>
   res.json(
     _.pick(packageJson, ['name', 'version', 'description', 'author', 'license'])
   )
+);
+
+// Upload image
+const allowTypes = process.env.ALLOW_TYPES.split(',').map(type => type.trim);
+const uploadConfig = {
+  fields: process.env.MAX_FIELD || 17,
+  files: process.env.MAX_FILE || 17,
+  fileSize: (process.env.MAX_SIZE || 100) * 1048576,
+  parts: process.env.MAX_PART || 17
+};
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, `${path.resolve(__dirname, 'public/resource')}`);
+  },
+  filename(req, { originalname, mimetype }, cb) {
+    const nameSegments = originalname.split('.');
+    const name = nameSegments[0] || `${Date.now()}`;
+
+    const mineTypeSegments = mimetype.split('/');
+    const ext = mineTypeSegments[1] || 'jpeg';
+    cb(null, `${Date.now()}-${name}.${ext}`);
+  }
+});
+const fileFilter = (req, { mimetype }, cb) =>
+  cb(null, Boolean(allowTypes.indexOf(mimetype) > -1));
+const uploader = multer({ storage, fileFilter, limits: uploadConfig });
+
+app.post('/upload', uploader.array('images'), (req, res) =>
+  res.json({ images: req.files })
 );
 
 const port = process.env.PORT || 9999;
